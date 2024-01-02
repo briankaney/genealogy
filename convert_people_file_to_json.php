7137c0ef37ec6bd64c0f85362da7d026a29504e3
briankaney <kaneybrian@gmail.com>
2024-01-01 21:00:04 -0600

    New json converter util

--- convert_people_file_to_json.php
+++ convert_people_file_to_json.php
@@ -5,27 +5,56 @@
 //   If run with no command line args, print out usage statement and bail
 //--------------------------------------------------------------------------------------
 
-  $argc = count($argv);
+  if($argc<2)
+  {
+    print "\n\nUsage:\n";
+    print "  convert_people_file_to_json.php family_tree_file\n\n";
+
+    print "Examples:\n";
+    print "  ./convert_people_file_to_json.php Kaney.people.txt\n\n";
+
+    print "  This scripts reads the contents of a people file.  A people file contains pipe\n";
+    print "  delimited text columns in a specific format described in the 'FormatNotes.people.txt' file.\n";
+    print "  This scripts converts all the data to a json.\n\n";
+
+    exit(0);
+  }
+
+//  include 'library_ancestory.php';
+
+//--------------------------------------------------------------------------------------
+//   Read the input file name and test to see if it exists.
+//--------------------------------------------------------------------------------------
+
+  $infile = $argv[1];
+
+  if(!file_exists($infile))
+  {
+    print "\nInput file not found: $infile\n\n";
+    exit(0);
+  }
 
 //--------------------------------------------------------------------------------------
 //   Read the required args and make sure the one that is the input is a file that 
 //--------------------------------------------------------------------------------------
-
-  $infile = "FamilyTree.10.txt";
-  $delimiter = "pipe";
+//Kaney|Edwin Frederick||M|2|6|7|09/13/1882|06/22/1950
+//Kaney|Henry|Jr|M|6|8|9|12/25/1848|10/11/1888
+//Kaney|Henry|Sr|M|8|0|0|03/09/1817|08/21/1899
+//Klemme|Anna Marie Elisabeth||F|1060|1065|1066|12/?/1755|03/07/1811
+//--------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------
 //   Read in contents of input file
-//Behrmann|Wilhelmina||F|11|Wilhelmina Behrmann|0|0|48|01/26/1834|47|10/05/1906|
-//Biesemeier|Frederick Simon||M|10|Frederick Simon Biesemeier|16|17|46|06/05/1822|47|10/25/1901|
 //--------------------------------------------------------------------------------------
 
   $lines = file("$infile",FILE_IGNORE_NEW_LINES);
   $num_lines = count($lines);
 
+  $last_name = Array();
+  $first_name = Array();
+  $name_suffix = Array();
   $gender = Array();
-  $id = Array();
-  $full_name = Array();
+  $index = Array();
   $father_id = Array();
   $mother_id = Array();
   $birth_date = Array();
@@ -37,13 +66,13 @@
 
     $last_name[$i] = $fields[0];
     $first_name[$i] = $fields[1];
-    $suffix[$i] = $fields[2];
+    $name_suffix[$i] = $fields[2];
     $gender[$i] = $fields[3];
-    $id[$i] = $fields[4];
+    $index[$i] = $fields[4];
     $father_id[$i] = $fields[5];
     $mother_id[$i] = $fields[6];
-    $birth_date[$i] = $fields[8];
-    $death_date[$i] = $fields[10];
+    $birth_date[$i] = $fields[7];
+    $death_date[$i] = $fields[8];
   }
   
  //--------------------------------------------------------------------------------------
@@ -53,11 +82,11 @@
   print ",";
   writeJSONArrayBlock("FirstName",$first_name,"string");
   print ",";
-  writeJSONArrayBlock("Suffix",$suffix,"string");
+  writeJSONArrayBlock("NameSuffix",$name_suffix,"string");
   print ",";
   writeJSONArrayBlock("Gender",$gender,"string");
   print ",";
-  writeJSONArrayBlock("ID",$id,"number");
+  writeJSONArrayBlock("Index",$index,"number");
   print ",";
   writeJSONArrayBlock("FatherID",$father_id,"number");
   print ",";
@@ -68,64 +97,8 @@
   writeJSONArrayBlock("DeathDate",$death_date,"string");
   print "}";
 
-
-
-
-
-/*
-  $self = ExtractSelfDataForID($lines,$target);
-
-  $gender = GetGenderFromData($self);
-  $str = GetNameStringFromData($self)." ".$gender;
-  print "\nSelf: $str\n\n";
-
 //--------------------------------------------------------------------------------------
-
-  $siblings = ExtractSiblingDataForID($lines,$target,"suppress_self");
-  $number_sibs = count($siblings);
-
-  print "\nBiological Siblings:\n  Number: $number_sibs\n\n";
-
-  for($i=0;$i<$number_sibs;++$i)
-  {
-    $str = "  ".GetNameStringFromData($siblings[$i]);
-    print "$str\n";
-  }
-
 //--------------------------------------------------------------------------------------
-
-  $children = ExtractChildrenDataForID($lines,$target);
-  $number_kids = count($children);
-	
-  print "\nBiological Children:\n  Number: $number_kids\n\n";
-
-  for($i=0;$i<$number_kids;++$i)
-  {
-    $str = "  ".GetNameStringFromData($children[$i]);
-    if($gender=="M") { $str = $str."  (with ".GetMotherIDFromData($children[$i]).")"; }
-    if($gender=="F") { $str = $str."  (with ".GetFatherIDFromData($children[$i]).")"; }
-    print "$str\n";
-  }
-
-//--------------------------------------------------------------------------------------
-
-  $ancestors = ExtractAncestorDataForID($lines,$target,10);
-
-  $num_gen = count($ancestors);
-
-  print "\n\n------------------------------------------\n\n";
-  for($g=1;$g<$num_gen;++$g)
-  {
-    for($i=0;$i<pow(2,$g);$i=$i+2)
-    {
-      $str = GetNameStringFromData($ancestors[$g][$i])."           ";
-      $str = $str.GetNameStringFromData($ancestors[$g][$i+1]);
-      print "$str\n";
-    }
-    print "\n";
-  }
-*/
-
 
   function writeJSONArrayBlock($name,$array,$type)
   {
